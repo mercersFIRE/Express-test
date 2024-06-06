899c2026627c40ce60d6a5b018232d041dbb926f
mercersFIRE <imranurrahman16@gmail.com>
2024-06-06 14:38:57 +0600

    setting up github in vs

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-let express= require("express");
-let app=express();
-app.use(express.urlencoded({extended:false}));
-function getWeather(req,res,next)
-{
-    req.var=true;
+let express = require("express");
+let app = express();
+app.use(express.urlencoded({ extended: false }));
+function getWeather(req, res, next) {
+    req.var = true;
     next();
 }
-app.get('/',getWeather,(req,res)=>{
+app.get('/', getWeather, (req, res) => {
     res.send(`
     <h1>Submit your answer</h1>
     <form action="/result" method="POST">
     <input type="text" name="color">
     <button>Submit</button>
     </form>
-    <p>${req.var?"YES":"NO"}</p>
+    <p>${req.var ? "YES" : "NO"}</p>
     `)
 })
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.send("Welcome to about")
 })
-app.post("/result" , (req,res)=>{
+app.post("/result", (req, res) => {
     res.send(`${req.body.color} is the entered data`);
 })
 
