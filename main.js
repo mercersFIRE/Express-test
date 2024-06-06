cf4f0c2dd506dd174e5a1fe4ab884544e357276f
mercersFIRE <imranurrahman16@gmail.com>
2024-06-06 14:27:25 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+let express= require("express");
+let app=express();
+app.use(express.urlencoded({extended:false}));
+function getWeather(req,res,next)
+{
+    req.var=true;
+    next();
+}
+app.get('/',getWeather,(req,res)=>{
+    res.send(`
+    <h1>Submit your answer</h1>
+    <form action="/result" method="POST">
+    <input type="text" name="color">
+    <button>Submit</button>
+    </form>
+    <p>${req.var?"YES":"NO"}</p>
+    `)
+})
+app.get('/about',(req,res)=>{
+    res.send("Welcome to about")
+})
+app.post("/result" , (req,res)=>{
+    res.send(`${req.body.color} is the entered data`);
+})
+
+app.listen(3000);
